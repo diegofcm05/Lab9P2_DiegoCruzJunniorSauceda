43bcf37d3bc5248d188d9fc54844ae54293169d2
Junnior Sauceda <junnior.sauceda05@unitec.edu.hn>
2023-09-15 15:00:25 -0600

    Creacion de la clase thread

---
+++ src/lab9p2_diegocruzjunniorsauceda/Hilo.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab9p2_diegocruzjunniorsauceda;
+
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import javax.swing.JProgressBar;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Junnior Sauceda
+ */
+public class Hilo extends Thread {
+    private Color color;
+    private JProgressBar jp;
+    private JTextArea Area=new JTextArea();
+    private String hola="";
+    private String Adios="";
+    private boolean vive;
+    private int avance;
+    
+    public Hilo(){
+        
+    }
+    public Hilo(Color c,JProgressBar J,JTextArea TA,String a,String h,int av){
+        this.color=c;
+        this.jp=J;
+        jp.setBackground(c);
+        this.Area=TA;
+        this.hola=h;
+        this.Adios=a;
+        this.avance=av;
+    }
+    public Hilo(Color c,JProgressBar J,String a,int av){
+        this.color=c;
+        this.jp=J;
+        jp.setBackground(c);
+        this.Adios=a;
+        this.avance=av;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public JProgressBar getJp() {
+        return jp;
+    }
+
+    public void setJp(JProgressBar jp) {
+        this.jp = jp;
+    }
+
+    public JTextArea getArea() {
+        return Area;
+    }
+
+    public void setArea(JTextArea Area) {
+        this.Area = Area;
+    }
+
+    public String getHola() {
+        return hola;
+    }
+
+    public void setHola(String hola) {
+        this.hola = hola;
+    }
+    
+    @Override
+    public void run(){
+        
+        while (vive) {            
+            if(jp.getValue()>=100){
+                Area.setText(hola);
+                JOptionPane.showMessageDialog(null, Adios);
+            }
+            else{
+               jp.setValue(jp.getValue()+avance);
+               
+            }
+        }
+    }
+}
