d4ddf71566f43101dd51c4e9eb2d66103996ecbd
Junnior Sauceda <junnior.sauceda05@unitec.edu.hn>
2023-09-15 17:39:01 -0600

    Metodo Agregar Terminado

--- src/lab9p2_diegocruzjunniorsauceda/Main.java
+++ src/lab9p2_diegocruzjunniorsauceda/Main.java
@@ -547,14 +547,14 @@ public class Main extends javax.swing.JFrame {
             try {
                 
                 db.query.execute("INSERT INTO TenRecordReal"
-                        + " ([Order ID], [Order Date], [Ship Date], [Ship Mode], [Customer ID], [Customer Name], Segment, Country, City, State, [Postal Code], Region, [Product ID], Category, [Sub-Category], [Product Name], Sales, Quantity, Discount, Profit)"
-                        + " VALUES ('" + OrderID + "', '" + OrderDate + "'+'" + ShipDate +"', '" + ShipMode + "', '" + CustomerID + "'+'" + CustomerName + "', '" + Segment + "'+'"+ Country + "', '" + City + "'+'" + State + "', '" + PostalCode + "'+'" + Region + "', '" + ProductID + "'+'" + Category + "', '" + SubCateg + "'+'" + ProductName + "', '" + Sales + "'" + "+'" + Quantity + "', '" + Discount + "'+'" + Profit + "')");
+                        + " ([Order ID], [Order Date], [Ship Date],[Ship Mode], [Customer ID], [Customer Name], Segment, Country, City, State, [Postal Code], Region, [Product ID], Category, [Sub-Category], [Product Name], Sales, Quantity, Discount, Profit )"
+                        + " VALUES ('" + OrderID + "', '" + OrderDate + "', '" + ShipDate + "', '" + ShipMode + "', '" + CustomerID + "', '" + CustomerName + "', '" + Segment + "', '" + Country + "', '" + City + "', '" + State + "', '" + PostalCode + "', '" + Region + "', '" + ProductID + "', '" + Category + "', '" + SubCateg + "', '" + ProductName + "', '" + Sales + "', '" + Quantity + "', '" + Discount + "', '" + Profit + "')");
                 db.commit();
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
             db.desconectar();
-
+            
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Ocurrio un error.");
             e.printStackTrace();
